fa224565d923133375c684d023a916e29bc878b4
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-07-26 12:07:45 -0400

    actualizacion de información 3

--- src/data/data.js
+++ src/data/data.js
@@ -14,7 +14,7 @@ export const datos_personales = {
   celular: '71299234',
   fotografia1: './miFoto.png',
   fotografia2: '',
-  cv: './Alexander-Bautista-(C.V.).pdf'
+  cv: './Alexander-Bautista-(CV).pdf'
 }
 
 //Contactos
